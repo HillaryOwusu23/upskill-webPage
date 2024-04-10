d64e6c3fd38123bca7eab83147d3886a5ef8d00c
Hillary owusu Ameyaw <124349494+HillaryOwusu23@users.noreply.github.com>
2024-04-09 20:45:02 -1200

    added storybook workspace and web workspace

--- apps/.storybook/main.js
+++ apps/.storybook/main.js
@@ -1,18 +1,17 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-
 import { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
 }
-const config: StorybookConfig = {
+
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
   stories: [
-    // "../stories/**/*.mdx",
-    "../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
@@ -22,12 +21,11 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-interactions"),
   ],
   framework: {
-    name: getAbsolutePath("@storybook/nextjs"),
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
   docs: {
     autodocs: "tag",
   },
-  staticDirs: ["..\\public"],
 };
 export default config;
